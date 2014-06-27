8861b9c369c011e88038c7b6f615f6c8467c356f
eric <nomad255@gmail.com>
2014-06-26 21:00:17 -0500

    static files

--- edgweb/settings.py
+++ edgweb/settings.py
@@ -173,6 +173,16 @@ DATABASES = { 'default' : dj_database_url.config(default='postgres://postgres:er
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Static asset configuration
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
+
 # try to load local_settings.py if it exists
 try: 
  from local_settings import *
